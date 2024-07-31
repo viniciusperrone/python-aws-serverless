3ac2cfdb87f31714bec71f164c1ba321b086664e
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-07-31 12:59:30 -0300

    feat: upload files with boto3

--- s3_with_boto3.py
+++ s3_with_boto3.py
@@ -8,11 +8,14 @@ load_dotenv()
 session = boto3.Session(profile_name=os.getenv('AWS_PROFILE_NAME'))
 client_s3 = session.client('s3')
 
+bucket_name = 'aws-python-course'
+bucket_region = 'us-east-2'
+
 try:
     response_bucket = client_s3.create_bucket(
-        Bucket='aws-python-course',
+        Bucket=bucket_name,
         CreateBucketConfiguration={
-            'LocationConstraint': 'us-east-2'
+            'LocationConstraint': bucket_region
         }
     )
 
@@ -20,4 +23,9 @@ try:
 except Exception as e:
     print('This bucket already exist!')
 
+response_upload = client_s3.upload_file(
+    'static_files/aws_logo_smile.png',
+    bucket_name,
+    'images/aws_logo_smile.png'
+)
 
