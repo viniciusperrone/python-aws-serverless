ef1335ba3114e3f0e62d2125866c78d85412b085
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-07-31 13:07:39 -0300

    feat: creation file from boto3

--- s3_with_boto3.py
+++ s3_with_boto3.py
@@ -23,9 +23,22 @@ try:
 except Exception as e:
     print('This bucket already exist!')
 
-response_upload = client_s3.upload_file(
+client_s3.upload_file(
     'static_files/aws_logo_smile.png',
     bucket_name,
     'images/aws_logo_smile.png'
 )
 
+spreadsheet = """
+    Name\tNome
+    Ana\t8
+    Mario\t9
+    Maria\t7
+    Carlos\t10
+"""
+
+client_s3.put_object(
+    Body=spreadsheet,
+    Bucket=bucket_name,
+    Key='names.xls'
+)
