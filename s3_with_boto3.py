35774c3790a979a79eee5e942836ce1669e6ef11
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-07-31 12:39:40 -0300

    feat: added try-catch statement on creation bucket

--- s3_with_boto3.py
+++ s3_with_boto3.py
@@ -8,11 +8,16 @@ load_dotenv()
 session = boto3.Session(profile_name=os.getenv('AWS_PROFILE_NAME'))
 client_s3 = session.client('s3')
 
-response_bucket = client_s3.create_bucket(
-    Bucket='aws-python-course',
-    CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-2'
-    }
-)
+try:
+    response_bucket = client_s3.create_bucket(
+        Bucket='aws-python-course',
+        CreateBucketConfiguration={
+            'LocationConstraint': 'us-east-2'
+        }
+    )
 
-print(response_bucket)
+    print(response_bucket)
+except Exception as e:
+    print('This bucket already exist!')
+
+
